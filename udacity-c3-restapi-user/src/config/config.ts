f3e6f452d8cd95cc6b384904e2c535abb451ac32
Wallace Wong <wallacewong82@gmail.com>
2019-09-15 20:08:45 +0800

    update: clean up code

--- udacity-c3-restapi-user/src/config/config.ts
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -9,15 +9,6 @@ export const config = {
     "aws_profile": process.env.AWS_PROFILE,
     "aws_media_bucket":process.env.AWS_MEDIA_BUCKET,
     "url": process.env.URL
-    // "jwt_secret": process.env.JWT_SECRET
-    // "username": "udagramwallydev",
-    // "password": "udagramwallydev",
-    // "database": "udagramwallydev",
-    // "host": "udagramwallydev.cuhbthreiifm.ap-southeast-1.rds.amazonaws.com",
-    // "dialect": "postgres",
-    // "aws_reigion": "ap-southeast-1",
-    // "aws_profile": "default",
-    // "aws_media_bucket": "udagramwallydev"
   },
   "prod": {
     "username": process.env.POSTGRESS_USERNAME,
